c3f99bbb18406dcbb990a9fb2e60da991e1967b6
roaudan <09nangy2@gmail.com>
2020-09-27 21:03:28 +0900

    Updates

---
+++ precache-manifest.7e36945fda6c39f7df145622bf42812f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9b5e541070063de77d6c603f22a51a",
+    "url": "/hachat/index.html"
+  },
+  {
+    "revision": "32b3d29359bf9d031da6",
+    "url": "/hachat/static/css/main.1c9e4056.chunk.css"
+  },
+  {
+    "revision": "4c20721d0259057395c9",
+    "url": "/hachat/static/js/2.04eeabc0.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/hachat/static/js/2.04eeabc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b3d29359bf9d031da6",
+    "url": "/hachat/static/js/main.5d082c8d.chunk.js"
+  },
+  {
+    "revision": "39475fff4fe884faadb4",
+    "url": "/hachat/static/js/runtime-main.a7586643.js"
+  }
+]);
